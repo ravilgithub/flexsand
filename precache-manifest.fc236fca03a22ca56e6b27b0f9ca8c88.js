778fbb6fc8d2428400677e1805888fd14ae2f946
Ravil <foragetonly800@mail.ru>
2020-12-27 20:11:13 +0300

    Initial dist subtree commit

---
+++ precache-manifest.fc236fca03a22ca56e6b27b0f9ca8c88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcce1b58ccfac21f6c2",
+    "url": "/flexsand/css/app.c3bb7454.css"
+  },
+  {
+    "revision": "6e0516f77f3246526a11",
+    "url": "/flexsand/css/chunk-74267a99.e802c0e3.css"
+  },
+  {
+    "revision": "6e267f55355369cacf60",
+    "url": "/flexsand/css/flexbox.63c3662b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/flexsand/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7aa183a6500566e190a534f2ee7235e7",
+    "url": "/flexsand/index.html"
+  },
+  {
+    "revision": "08a509755f7045be721b",
+    "url": "/flexsand/js/about.46b9d4bc.js"
+  },
+  {
+    "revision": "0dcce1b58ccfac21f6c2",
+    "url": "/flexsand/js/app.2526dc8a.js"
+  },
+  {
+    "revision": "6e0516f77f3246526a11",
+    "url": "/flexsand/js/chunk-74267a99.32fc87c1.js"
+  },
+  {
+    "revision": "cc6438742f707126544e",
+    "url": "/flexsand/js/chunk-vendors.4527ee3e.js"
+  },
+  {
+    "revision": "6e267f55355369cacf60",
+    "url": "/flexsand/js/flexbox.337cbc18.js"
+  },
+  {
+    "revision": "2a5d92282f10bb83af99958f50fd6249",
+    "url": "/flexsand/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/flexsand/robots.txt"
+  }
+]);
